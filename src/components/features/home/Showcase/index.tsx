7f85e981988262ce018da83ab3ff7bc60fa1abdb
Fnrzz <faridnurrganteng123@gmail.com>
2026-04-06 15:45:41 +0700

    [PUSH] fixing bug

--- src/components/features/home/Showcase/index.tsx
+++ src/components/features/home/Showcase/index.tsx
@@ -9,7 +9,7 @@ const Showcase = () => {
     <section className="flex flex-col gap-8 w-full h-full py-10 px-6 lg:p-15">
       <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-5">
         <div className="w-full md:w-[50%] lg:w-[40%]" data-aos="fade-right">
-          <h2 className="text-3xl/10 lg:text-4xl/12 font-light tracking-wide md:text-left">
+          <h2 className="text-3xl/10 lg:text-4xl/12 font-light tracking-wide text-center md:text-left">
             <span className="font-semibold">Your primary home</span> might begin
             to feel left out.
           </h2>
